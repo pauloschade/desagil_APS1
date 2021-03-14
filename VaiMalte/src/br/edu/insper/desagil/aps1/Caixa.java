9391db87113bddaefa49a20f041591edb4c51e23
pauloschade <paulosc4@al.insper.edu.br>
2021-03-14 11:40:20 -0300

    Caixa e Pedido

---
+++ VaiMalte/src/br/edu/insper/desagil/aps1/Caixa.java
@@ -0,0 +1,16 @@
+package br.edu.insper.desagil.aps1;
+
+import java.util.Map;
+
+import Produtos.codigo;
+
+public class Caixa {
+	private Map<Produtos.codigo, Int> descontos;
+	
+	public Caixa() {
+		super();
+		this.descontos = HashMap<Produtos.codigo, Int>();
+	}
+	
+	
+}
